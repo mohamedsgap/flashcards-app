de244452ba47f507db17fdb51c9e89bd2c7afe6b
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-10-25 08:11:08 +0200

    create helpers.js for handling app notifcation

---
+++ utils/helpers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AsyncStorage } from 'react-native';
+import { Notifications } from 'expo';
+
+import * as Permissions from 'expo-permissions';
+
+const NOTIFICATION_KEY = 'MobileFlashcard:notifications';
+const CHANNEL_ID = 'DailyReminder';
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+    Notifications.cancelAllScheduledNotificationsAsync
+  );
+}
