f2c523dc2f03d30e3e1258f6e726d4942fefd48a
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-11-01 17:44:09 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index';
+import Constants from 'expo-constants';
+import AppNavigator from './navigation/AppNavigator';
+import { setLocalNotification } from './utils/helpers';
 
-export default function App() {
+const store = createStore(
+  reducer /* preloadedState, */,
+  applyMiddleware(thunk, logger)
+);
+
+function FlashcardStatusBar({ backgroundColor, ...props }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </View>
   );
+}
+FlashcardStatusBar.propTypes = {
+  backgroundColor: PropTypes.string.isRequired
+};
+
+export default class App extends React.Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <FlashcardStatusBar
+            backgroundColor="green"
+            barStyle="light-content"
+          />
+          <AppNavigator />
+        </View>
+      </Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#dde'
+  }
 });
